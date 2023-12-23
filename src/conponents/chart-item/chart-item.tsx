f7ef22c02f99ce69b6e638294953260a04081a8c
AndMed <andmed20@yandex.ru>
2023-12-23 20:19:51 +0300

    Fix slow rendering

---
+++ src/conponents/chart-item/chart-item.tsx
@@ -0,0 +1,67 @@
+import { Paper, Box, Button, Typography, Skeleton } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import CandleChart from '../candle-chart'
+import { ICoinCandlesStat } from '../../types/models'
+import { useInView } from 'react-intersection-observer'
+
+interface IProps {
+  item: ICoinCandlesStat
+  index: number
+}
+
+const ChartItem: React.FC<IProps> = ({ item, index }) => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  })
+
+  return (
+    <Paper
+      ref={ref}
+      elevation={2}
+      style={{ padding: 5, width: 300, height: 225 }}
+    >
+      {!inView ? (
+        <Skeleton variant='rectangular' width={'100%'} height={'100%'} />
+      ) : (
+        <>
+          <Box
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Link
+              to={`https://www.bybit.com/trade/usdt/${item.result.symbol}`}
+              target='_blank'
+            >
+              <Button>
+                <Typography style={{ paddingLeft: 10 }}>
+                  {item.result.symbol}
+                </Typography>
+              </Button>
+            </Link>
+            <Typography
+              style={{
+                paddingRight: 10,
+                fontWeight: 400,
+                color:
+                  item.custom?.differencePercent &&
+                  item.custom?.differencePercent > 0
+                    ? 'green'
+                    : 'red',
+              }}
+            >
+              {item.custom?.differencePercent}
+            </Typography>
+          </Box>
+          <CandleChart item={item} />
+        </>
+      )}
+    </Paper>
+  )
+}
+
+export default ChartItem
--- src/conponents/charts-list.tsx
+++ src/conponents/charts-list.tsx
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react'
 import { ICoinCandlesStat } from '../types/models'
-import CandleChart from './candle-chart'
-import { Box, Button, Paper, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Box } from '@mui/material'
 import { AppContext } from '../store/context'
+import ChartItem from './chart-item/chart-item'
 
 interface IProps {
   candlesList: ICoinCandlesStat[]
@@ -25,43 +24,8 @@ const ChartsList: React.FC<IProps> = ({ candlesList }) => {
         gap: 10,
       }}
     >
-      {candlesList.map((item) => {
-        return (
-          <Paper key={item.result.symbol} elevation={2} style={{ padding: 5 }}>
-            <Box
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}
-            >
-              <Link
-                to={`https://www.bybit.com/trade/usdt/${item.result.symbol}`}
-                target='_blank'
-              >
-                <Button>
-                  <Typography style={{ paddingLeft: 10 }}>
-                    {item.result.symbol}
-                  </Typography>
-                </Button>
-              </Link>
-              <Typography
-                style={{
-                  paddingRight: 10,
-                  fontWeight: 400,
-                  color:
-                    item.custom?.differencePercent &&
-                    item.custom?.differencePercent > 0
-                      ? 'green'
-                      : 'red',
-                }}
-              >
-                {item.custom?.differencePercent}
-              </Typography>
-            </Box>
-            <CandleChart item={item} />
-          </Paper>
-        )
+      {candlesList.map((item, index) => {
+        return <ChartItem key={item.result.symbol} item={item} index={index} />
       })}
     </Box>
   )
