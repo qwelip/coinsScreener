52922df0bf5abbd2f2d3003e7c356c6f19e7f53e
AndMed <andmed20@yandex.ru>
2024-01-18 08:34:23 +0300

    Add default 0 candles, 1 min %

--- src/pages/charts-page.tsx
+++ src/pages/charts-page.tsx
@@ -5,17 +5,13 @@ import ChartsList from '../conponents/charts-list'
 import { AppContext } from '../store/context'
 import Filter from '../conponents/filter'
 import { ICoinCandlesStat, Interval } from '../types/models'
-import {
-  getIntervalTitle,
-  getSortedPercentGrowCandlesStat,
-} from '../utils/utils'
+import { getIntervalTitle, getSortedPercentGrowCandlesStat } from '../utils/utils'
 import { timeIntervals } from '../common/common-data'
 import LoadingBackdrop from '../common/loading-backdrop'
 
 const ChartsPage = () => {
   let resCandlesData: ICoinCandlesStat[] | undefined = []
-  const { isLoading, candlesData, interval, setInterval } =
-    useContext(AppContext)
+  const { isLoading, candlesData, interval, setInterval } = useContext(AppContext)
 
   const [candlesToCheck, setCandlesToCheck] = useState(9)
   const [minProcToShow, setMinProcToShow] = useState(5)
@@ -31,10 +27,7 @@ const ChartsPage = () => {
     resCandlesData = candlesData
   }
   if (!isLoading && candlesData && candlesData.length > 0) {
-    const withPercent = getSortedPercentGrowCandlesStat(
-      candlesData,
-      candlesToCheck
-    )
+    const withPercent = getSortedPercentGrowCandlesStat(candlesData, candlesToCheck)
     resCandlesData = withPercent.filter((i) => {
       if (minProcToShow === 0) {
         return true
@@ -46,8 +39,8 @@ const ChartsPage = () => {
 
   useEffect(() => {
     if (!interval) return
-    setCandlesToCheck(9)
-    setMinProcToShow(5)
+    setCandlesToCheck(0)
+    setMinProcToShow(1)
   }, [interval])
 
   return (
